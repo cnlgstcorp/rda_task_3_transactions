84ca9793c869a0106dd5dda19bfa9274ad8c54ab
Ioann Nadtocheev <ioannnatocheev@gmail.com>
2025-04-16 16:38:52 +0300

    Add transaction logic for creating order and updating product stock

--- task.sql
+++ task.sql
@@ -1,11 +1,23 @@
--- Use our database
-USE ShopDB; 
+USE ShopDB;
 
--- Some data should be created outside the transaction (here)
+-- Добавляем заказ вне транзакции
+INSERT INTO Orders (CustomerID, Date)
+VALUES (1, '2023-01-01');
 
--- Start the transaction 
-START TRANSACTION; 
+-- Сохраняем ID созданного заказа
+SET @order_id = LAST_INSERT_ID();
 
--- And some data should be created inside the transaction 
+-- Начинаем транзакцию
+START TRANSACTION;
 
-COMMIT; 
+-- Уменьшаем количество товара на складе
+UPDATE Products
+SET WarehouseAmount = WarehouseAmount - 1
+WHERE ID = 1;
+
+-- Добавляем товар в заказ
+INSERT INTO OrderItems (OrderID, ProductID, Count)
+VALUES (@order_id, 1, 1);
+
+-- Завершаем транзакцию
+COMMIT;
